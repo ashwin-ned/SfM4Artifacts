b81b89ad78341c5305efb8d653849e0d2769f7a4
Ashwin Nedungadi <53474640+ashwin-ned@users.noreply.github.com>
2024-02-24 00:56:01 +0100

    Updated the python version

---
+++ python/pipeline.py
@@ -0,0 +1,34 @@
+import os
+import numpy as np
+import cv2
+import open3d
+
+
+
+
+def dataloader(path):
+    images = list()
+    for filename in os.listdir(path):
+        print(f"Reading file {filename}")
+        if filename.endswith(".jpg" or ".png"):
+            image = cv2.imread(os.path.join(path, filename))
+            images.append(image) 
+            resized_image = cv2.resize(image, (500, 500), interpolation = cv2.INTER_AREA)
+
+            cv2.imshow('image', resized_image)
+            cv2.waitKey(0)        
+   
+    print(f"Loaded {len(images)} images")
+    
+    return images 
+
+
+if __name__ ==  "__main__":
+    # Load Data
+    images = dataloader('C:/Users/Ashwin/Desktop/SfM Project/Sfm4Artifacts/datasets/laocoon/')
+
+    # Run the preprocessing pipeline
+
+    # Run the Reconstruction Pipeline 
+
+    # Visualize the results
